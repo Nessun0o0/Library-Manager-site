2cb253de8975e10a2c04fa295c03c4de153749ff
Nessun0o0 <francescopalomb@gmail.com>
2023-02-18 18:50:24 +0100

    Updates

---
+++ precache-manifest.2ce156f8c50fe00ede1d71871fb7a9f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef82afe55a91c4b36ce8",
+    "url": "/Library-Manager-site/static/css/main.87f66cec.chunk.css"
+  },
+  {
+    "revision": "ef82afe55a91c4b36ce8",
+    "url": "/Library-Manager-site/static/js/main.ef82afe5.chunk.js"
+  },
+  {
+    "revision": "b965d220e0d6421fd4e0",
+    "url": "/Library-Manager-site/static/js/1.b965d220.chunk.js"
+  },
+  {
+    "revision": "5fb8800758d0f0a2159f",
+    "url": "/Library-Manager-site/static/js/2.5fb88007.chunk.js"
+  },
+  {
+    "revision": "7919c3dc220e0332f5c9",
+    "url": "/Library-Manager-site/static/js/runtime~main.7919c3dc.js"
+  },
+  {
+    "revision": "1a71aa6724d856a44b48ff858513bb40",
+    "url": "/Library-Manager-site/index.html"
+  }
+];
